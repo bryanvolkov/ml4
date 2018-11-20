ff70b5daee32ed323f8f2b8d8ae22fff6d60cf98
bryanvolkov <33441916+bryanvolkov@users.noreply.github.com>
2018-11-19 18:17:20 -0800

    Add files via upload

---
+++ prog.py
@@ -0,0 +1,146 @@
+import os
+import sys
+import random
+import warnings
+
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+from tqdm import tqdm
+from itertools import chain
+from skimage.io import imread, imshow, imread_collection, concatenate_images
+from skimage.transform import resize
+from skimage.morphology import label
+
+from keras.models import Model, load_model
+from keras.layers import Input
+from keras.layers.core import Dropout, Lambda
+from keras.layers.convolutional import Conv2D, Conv2DTranspose
+from keras.layers.pooling import MaxPooling2D
+from keras.layers.merge import concatenate
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras import backend as K
+
+import tensorflow as tf
+
+# Set some parameters
+IMG_WIDTH = 256
+IMG_HEIGHT = 256
+IMG_CHANNELS = 3
+TRAIN_IMAGES_PATH = 'training_images/'
+TRAIN_MASKS_PATH = 'training_masks/'
+VAL_IMAGES_PATH = 'validation_images'
+VAL_MASKS_PATH = 'validation_masks'
+TEST_PATH = 'testing_images'
+
+warnings.filterwarnings('ignore', category=UserWarning, module='skimage')
+seed = 42
+random.seed = seed
+np.random.seed = seed
+
+# Get train and test IDs
+train_images_ids = next(os.walk(TRAIN_IMAGES_PATH))[2] # list
+train_masks_ids = next(os.walk(TRAIN_MASKS_PATH))[2] # list
+test_images_ids = next(os.walk(TEST_PATH))[2] # list
+
+
+#print(type(train_ids))
+print(train_images_ids[156])
+print(train_masks_ids[156])
+print('images ids: ' + str(len(train_images_ids)))
+print('masks ids: ' + str(len(train_masks_ids)))
+
+
+#from scipy.misc import imsave
+# Get and resize train images and masks
+X_train = np.zeros((len(train_images_ids), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
+Y_train = np.zeros((len(train_masks_ids), IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool)
+print('Getting and resizing train images and masks ... ')
+sys.stdout.flush()
+for n, id_ in tqdm(enumerate(train_images_ids), total=len(train_images_ids)):
+    path = 'resized_training_images/' + id_
+    img = imread(path)[:,:,:IMG_CHANNELS]
+    X_train[n] = img
+
+for n, id_ in tqdm(enumerate(train_masks_ids), total=len(train_masks_ids)):
+    path = 'resized_training_masks/' + id_
+    mask_ = imread(path)
+    mask = np.expand_dims(mask_, axis=-1)
+    Y_train[n] = mask
+    
+# Get and resize test images
+X_test = np.zeros((len(test_images_ids), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
+sizes_test = []
+print('Getting and resizing test images ... ')
+sys.stdout.flush()
+for n, id_ in tqdm(enumerate(test_images_ids), total=len(test_images_ids)):
+    path = TEST_PATH + '/' + id_
+    img = imread(path)[:,:,:IMG_CHANNELS]
+    sizes_test.append([img.shape[0], img.shape[1]])
+    img = resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
+    X_test[n] = img
+
+print('Done!')
+
+
+# Define IoU metric
+def mean_iou(y_true, y_pred):
+    prec = []
+    for t in np.arange(0.5, 1.0, 0.05):
+        y_pred_ = tf.to_int32(y_pred > t)
+        score, up_opt = tf.metrics.mean_iou(y_true, y_pred_, 2)
+        K.get_session().run(tf.local_variables_initializer())
+        with tf.control_dependencies([up_opt]):
+            score = tf.identity(score)
+        prec.append(score)
+    return K.mean(K.stack(prec), axis=0)
+
+
+# Predict on train, val and test
+model = load_model('model-dsbowl2018-1.h5', custom_objects={'mean_iou': mean_iou})
+#preds_train = model.predict(X_train[:int(X_train.shape[0]*0.9)], verbose=1)
+#preds_val = model.predict(X_train[int(X_train.shape[0]*0.9):], verbose=1)
+preds_test = model.predict(X_test, verbose=1)
+
+# Threshold predictions
+#preds_train_t = (preds_train > 0.5).astype(np.uint8)
+#preds_val_t = (preds_val > 0.5).astype(np.uint8)
+preds_test_t = (preds_test > 0.5).astype(np.uint8)
+
+# Create list of upsampled test masks
+preds_test_upsampled = []
+for i in range(len(preds_test)):
+    preds_test_upsampled.append(resize(np.squeeze(preds_test[i]), 
+                                       (sizes_test[i][0], sizes_test[i][1]), 
+                                       mode='constant', preserve_range=True))
+
+# Run-length encoding stolen from https://www.kaggle.com/rakhlin/fast-run-length-encoding-python
+def rle_encoding(x):
+    dots = np.where(x.T.flatten() == 1)[0]
+    run_lengths = []
+    prev = -2
+    for b in dots:
+        if (b>prev+1): run_lengths.extend((b + 1, 0))
+        run_lengths[-1] += 1
+        prev = b
+    return run_lengths
+
+def prob_to_rles(x, cutoff=0.5):
+    lab_img = label(x > cutoff)
+    for i in range(1, lab_img.max() + 1):
+        yield rle_encoding(lab_img == i)
+
+new_test_ids = []
+rles = []
+for n, id_ in enumerate(test_ids):
+    rle = list(prob_to_rles(preds_test_upsampled[n]))
+    rles.extend(rle)
+    new_test_ids.extend([id_] * len(rle))
+
+# Create submission DataFrame
+sub = pd.DataFrame()
+sub['ImageId'] = new_test_ids
+sub['EncodedPixels'] = pd.Series(rles).apply(lambda x: ' '.join(str(y) for y in x))
+sub.to_csv('sub-dsbowl2018-1.csv', index=False)
